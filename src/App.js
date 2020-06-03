0132f79d231830a274675c8b8049a4d44bb4cb09
Warren Simpson <warren.simpson34@gmail.com>
2020-06-03 01:10:18 -0400

    Updated last two cards to have different information

--- src/App.js
+++ src/App.js
@@ -14,16 +14,16 @@ class App extends Component {
         email="john@gmail.com"
         />
 
-       <ContactCard name="John" 
-       mobile="5346782345" 
-       work="5437862356" 
-       email="john@gmail.com"
+       <ContactCard name="Peter" 
+       mobile="3306350034" 
+       work="5437866473" 
+       email="peter@gmail.com"
       />
 
-      <ContactCard name="John" 
-       mobile="5346782345" 
-       work="5437862356" 
-       email="john@gmail.com"
+      <ContactCard name="Alex" 
+       mobile="5346787790" 
+       work="8097862356" 
+       email="alex@gmail.com"
       />
       </>
     );
