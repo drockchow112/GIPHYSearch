120b1da2ababbe199c5211467cdb0ce0b598b9a6
Derrick <derrick.chow234@gmail.com>
2020-06-05 15:28:47 -0400

    Added limit

--- src/components/Giphy.js
+++ src/components/Giphy.js
@@ -1,92 +1,91 @@
 import React, { Component } from "react";
-import axios from "axios"
-import Search from "./Search"
+import axios from "axios";
+import Search from "./Search";
 
 class GIPHY extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {word: this.props.word, giphies:[], search: this.props.search};
-    }
-
-    componentDidMount() {
-      const API_KEY = process.env.REACT_APP_API_KEY;
-      const urlTrending = `http://api.giphy.com/v1/gifs/trending?&api_key=${API_KEY}`;
-  
-      if(this.state.search === false) {
-        axios
-          .get(urlTrending, { api_key: API_KEY })
-          .then(response => {
-            // const giphy = this.props.giphy;
-            const giphies = response.data.data;
-            console.log(giphies);
-            this.setState({ word: this.props.word, giphies });
-          })
-          .catch(err => console.log(err));
-      } 
-    }
-  
-    componentDidUpdate(prevProps) {
-      const API_KEY = process.env.REACT_APP_API_KEY;
-      const urlSearch = `http://api.giphy.com/v1/gifs/search?q=${this.state.word}&api_key=${API_KEY}`;
-      const urlTrending = `http://api.giphy.com/v1/gifs/trending?&api_key=${API_KEY}`;
-
-
-
-      // if (prevProps.search === this.props.search) { 
-      //   axios
-      //     .get(urlTrending, { api_key: API_KEY })
-      //     .then(response => {
-      //       // const giphy = this.props.giphy;
-      //       const giphies = response.data.data;
-      //       console.log(giphies);
-      //       this.setState({ word: this.props.word, giphies });
-      //     })
-      //     .catch(err => console.log(err));
-      // } 
-
-      if (prevProps.word !== this.props.word) {
-        let word = this.props.word;
-        axios
-          .get(
-            `http://api.giphy.com/v1/gifs/search?q=${word}&api_key=${API_KEY}`,
-            { api_key: API_KEY }
-          )
-          .then(response => {
-            // const giphy = this.props.giphy;
-            const giphies = response.data.data;
-            console.log(giphies);
-            this.setState({ word: this.props.word, giphies });
-          })
-          .catch(err => console.log(err));
-      } else {
-        console.log("False");
-      }
-    }
-
-    handleChange = (e) => {
-      this.setState({word: e.target.value });
+  constructor(props) {
+    super(props);
+    this.state = {
+      word: this.props.word,
+      giphies: [],
+      search: this.props.search,
+      limit: this.props.limit
     };
+  }
 
-    
+  componentDidMount() {
+    const API_KEY = process.env.REACT_APP_API_KEY;
+    const urlTrending = `http://api.giphy.com/v1/gifs/trending?&api_key=${API_KEY}`;
 
-    render() {
-      
-
-      return (
-        <div> 
-          
-          {/* <img src={this.state.giphies[0].images[0].downsized_large.url}/> */}
-
-            {this.state.giphies.map((giph) => (
-                <li>
-                    <p>{giph.id}</p>
-                    <img src={giph.images.fixed_height_small.url}></img>
-                </li>
-            ))}
-
-        </div>
-      );
+    if (this.state.search === false) {
+      axios
+        .get(urlTrending, { api_key: API_KEY })
+        .then(response => {
+          // const giphy = this.props.giphy;
+          const giphies = response.data.data;
+          console.log(giphies);
+          this.setState({ word: this.props.word, giphies });
+        })
+        .catch(err => console.log(err));
     }
   }
 
-  export default GIPHY;
+  componentDidUpdate(prevProps) {
+    const API_KEY = process.env.REACT_APP_API_KEY;
+    const urlSearch = `http://api.giphy.com/v1/gifs/search?q=${this.state.word}&api_key=${API_KEY}`;
+    const urlTrending = `http://api.giphy.com/v1/gifs/trending?&api_key=${API_KEY}`;
+
+    // if (prevProps.search === this.props.search) {
+    //   axios
+    //     .get(urlTrending, { api_key: API_KEY })
+    //     .then(response => {
+    //       // const giphy = this.props.giphy;
+    //       const giphies = response.data.data;
+    //       console.log(giphies);
+    //       this.setState({ word: this.props.word, giphies });
+    //     })
+    //     .catch(err => console.log(err));
+    // }
+
+    if (
+      prevProps.word !== this.props.word ||
+      prevProps.limit !== this.props.limit
+    ) {
+      let limit = this.props.limit;
+      let word = this.props.word;
+      axios
+        .get(
+          `http://api.giphy.com/v1/gifs/search?q=${word}&limit=${limit}&api_key=${API_KEY}`,
+          { api_key: API_KEY }
+        )
+        .then(response => {
+          // const giphy = this.props.giphy;
+          const giphies = response.data.data;
+          console.log(giphies);
+          this.setState({ word: this.props.word, giphies });
+        })
+        .catch(err => console.log(err));
+    }
+  }
+
+  handleChange = e => {
+    this.setState({ word: e.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        {/* <img src={this.state.giphies[0].images[0].downsized_large.url}/> */}
+
+        {this.state.giphies.map(giph => (
+          <li>
+            <p>{giph.id}</p>
+            <img src={giph.images.fixed_height_small.url}></img>
+          </li>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default GIPHY;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,55 +1,76 @@
-import React, {Component} from "react"
+import React, { Component } from "react";
 import GIPHY from "./Giphy";
 
-
 class Search extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {word: "", search: false}
+  constructor(props) {
+    super(props);
+    this.state = { word: "", search: false, limit: 5 };
+  }
+
+  handleChange = e => {
+    console.log(e.target.value);
+    this.setState({
+      [e.target.name]: e.target.value,
+      search: false
+    });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    if (this.state.word !== null) {
+      this.setState({ search: true });
     }
+  };
 
-    handleChange = (e) => {
-        console.log(e.target.value);
-        this.setState({
-            word: e.target.value,
-            search: false
-        })
-    }
+  // showGIPHY()  {
+  //     if (this.state.search === true) {
+  //         return (
+  //             <GIPHY word = {this.state.word}/>
+  //         )
+  //     }
+  // }
 
-    onSubmit = (e) =>  {
-        e.preventDefault()  
-        if (this.state.word !== null) {
-            this.setState({search:true});
-
-        }
-    }
-
-    // showGIPHY()  {
-    //     if (this.state.search === true) {
-    //         return (
-    //             <GIPHY word = {this.state.word}/>
-    //         )
-    //     }
-    // }
-
-    render() {
-    
-        return (
-          <>
-            <form>
-              <input
-                type="text"
-                placeholder="Search.."
-                onChange={this.handleChange}
-              ></input>
-              <button type="submit" onClick={this.onSubmit}>
-                Search
-              </button>
-            </form>
-            <GIPHY word={this.state.word} search={this.state.search}/>
-          </>
-        );
-    }
+  render() {
+    return (
+      <>
+        <form>
+          <input
+            type="text"
+            placeholder="Search.."
+            name="word"
+            onChange={this.handleChange}
+          ></input>
+          <input
+            type="number"
+            placeholder="Limit"
+            name="limit"
+            onChange={this.handleChange}
+          />
+          {/* <input
+            type="checkbox"
+            name="g"
+            onChange={this.handleChange}
+            checked
+          /> */}
+          {/* <label for="g">G</label> */}
+          {/* <input type="checkbox" name="pg" onChange={this.handleChange} />
+          <label for="pg">PG</label>
+          <input type="checkbox" name="pg-13" onChange={this.handleChange} />
+          <label for="pg-13">PG-13</label>
+          <input type="checkbox" name="r" onChange={this.handleChange} />
+          <label for="r">R</label> */}
+          <button type="submit" onClick={this.onSubmit}>
+            Search
+          </button>
+        </form>
+        <GIPHY
+          word={this.state.word}
+          search={this.state.search}
+          limit={this.state.limit}
+        />
+      </>
+    );
+  }
 }
 
 export default Search;
